447a12f0e5d93f28ffd5629fcf6e386a649d3a5d
techcaptain04 <andrii.seliuk@outlook.com>
2026-02-01 19:41:30 -0500

    fix: udpate gallery route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
-import { Route, Navigate, Routes } from "react-router-dom";
-import "./App.css";
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
 import 'react-toastify/dist/ReactToastify.css';
 // import Login from "./components/login";
 // import Callback from "./components/callback";
@@ -8,51 +8,51 @@ import 'react-toastify/dist/ReactToastify.css';
 // import ProjectTasks from "./components/tasks";
 // import axios from "axios";
 // import { useLocalStorage } from "./hooks/useLocalstorage";
-import { ToastContainer } from "react-toastify";
-import Login from "./pages/login";
+import { ToastContainer } from 'react-toastify';
+import Login from './pages/login';
 // import Signup from "./pages/signup";
-import Admin from "./pages/admin";
-import Gallery from "./pages/admin/screenshots";
-import Nav from "./components/nav";
-import ProtectedRoute from "./protected-route";
+import Admin from './pages/admin';
+import Gallery from './pages/admin/screenshots';
+import Nav from './components/nav';
+import ProtectedRoute from './protected-route';
 function App() {
-    return (
-        <div className="bg-white App text-slate-500 dark:text-slate-400 dark:bg-slate-900 min-h-[100vh]">
-            <Routes>
-                <Route path="/login" element={<Login/>}/>
-                {/* <Route path="/signup" element={<Signup/>}/> */}
-                {/* <Route path="/" element={<Admin/>}/>
+  return (
+    <div className="bg-white App text-slate-500 dark:text-slate-400 dark:bg-slate-900 min-h-[100vh]">
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        {/* <Route path="/signup" element={<Signup/>}/> */}
+        {/* <Route path="/" element={<Admin/>}/>
                 <Route path="/:userId/gallery" element={<Gallery/>}/> */}
-                <Route 
-                    path="/" 
-                    element={
-                        <ProtectedRoute>
-                            <Nav/>
-                            <Admin />
-                        </ProtectedRoute>
-                    } 
-                />
-                <Route 
-                    path="/:userId/gallery" 
-                    element={
-                        <ProtectedRoute>
-                            <Nav/>
-                            <Gallery/>
-                        </ProtectedRoute>
-                    } 
-                />
-                {/* <Route path="/"      element={<Admin/>}/> */}
-                {/* <Route path="/login" element={<Login />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Nav />
+              <Admin />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/:username/gallery"
+          element={
+            <ProtectedRoute>
+              <Nav />
+              <Gallery />
+            </ProtectedRoute>
+          }
+        />
+        {/* <Route path="/"      element={<Admin/>}/> */}
+        {/* <Route path="/login" element={<Login />} />
                 <Route path="/" element={<Navigate replace to="/login" />} />
                 <Route path="/callback" element={<Callback />} />
                 <Route path="/projects" element={<Portals />} />
                 <Route path="/portals/:boardID/tasks" element={<ProjectTasks />} />
                 <Route path=":portalId/projects" element={<Projects />} /> */}
-                {/* <Route path="/portals/:portalId/projects/:projectId/tasks" element={<ProjectTasks />} /> */}
-            </Routes>
-            <ToastContainer />
-        </div>
-    );
+        {/* <Route path="/portals/:portalId/projects/:projectId/tasks" element={<ProjectTasks />} /> */}
+      </Routes>
+      <ToastContainer />
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/components/date-picker/index.tsx
+++ frontend/src/components/date-picker/index.tsx
@@ -1,7 +1,6 @@
-
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 import { Calendar } from 'primereact/calendar';
-import { Nullable } from "primereact/ts-helpers";
+import { Nullable } from 'primereact/ts-helpers';
 import { Button } from 'primereact/button';
 
 interface DatePickerProps {
@@ -9,57 +8,65 @@ interface DatePickerProps {
 }
 
 const Datepicker: React.FC<DatePickerProps> = ({ onDateClick }) => {
-    const [date, setDate] = useState<Nullable<Date>>(null);
+  const [date, setDate] = useState<Nullable<Date>>(null);
 
-    useEffect(() => {
-        console.log('hello', date);
-        
-        if (date) {
-            if (onDateClick) {
-                onDateClick(date);
-            }
-        } else {
-            const newDate = new Date();
-            console.log("new: ", newDate);
-            setDate(newDate);
-            if (onDateClick) {
-                onDateClick(date);
-            }
-        }
-    }, [date]);
+  useEffect(() => {
+    console.log('hello', date);
 
-    const increaseDate = () => {
-        if (date) {
-            const newDate = new Date(date);
-            newDate.setDate(newDate.getDate() + 1); // Increase date by 1 day
-            setDate(newDate);
-        }
-    };
+    if (date) {
+      if (onDateClick) {
+        onDateClick(date);
+      }
+    } else {
+      const newDate = new Date();
+      console.log('new: ', newDate);
+      setDate(newDate);
+      if (onDateClick) {
+        onDateClick(date);
+      }
+    }
+  }, [date]);
 
-    const decreaseDate = () => {
-        if (date) {
-            const newDate = new Date(date);
-            newDate.setDate(newDate.getDate() - 1); // Decrease date by 1 day
-            setDate(newDate);
-        }
-    };
+  const increaseDate = () => {
+    if (date) {
+      const newDate = new Date(date);
+      newDate.setDate(newDate.getDate() + 1); // Increase date by 1 day
+      setDate(newDate);
+    }
+  };
 
-    return (
-        
-        <div className="card pl-8 pt-6">
-            <div className="flex-auto">
-                <label htmlFor="buttondisplay" className="font-bold block mb-2">
-                    Pick Date
-                </label>
-                <div className="flex items-center space-x-2"> {/* Adjusted for spacing */}
-                    <Button icon="pi pi-arrow-left" className="p-button-rounded"  onClick={decreaseDate} />
-                    <Calendar value={date} onChange={(e) => setDate(e.value)} showIcon />
-                    <Button icon="pi pi-arrow-right" className="p-button-rounded"  onClick={increaseDate} />
-                </div>
-            </div>
+  const decreaseDate = () => {
+    if (date) {
+      const newDate = new Date(date);
+      newDate.setDate(newDate.getDate() - 1); // Decrease date by 1 day
+      setDate(newDate);
+    }
+  };
+
+  return (
+    <div className="card pl-8 pt-6">
+      <div className="flex-auto">
+        <label htmlFor="buttondisplay" className="font-bold block mb-2">
+          Pick Date
+        </label>
+        <div className="flex items-center space-x-2">
+          {' '}
+          {/* Adjusted for spacing */}
+          <Button
+            icon="pi pi-arrow-left"
+            className="p-button-rounded"
+            onClick={decreaseDate}
+          />
+          <Calendar value={date} onChange={(e) => setDate(e.value)} showIcon />
+          <Button
+            icon="pi pi-arrow-right"
+            className="p-button-rounded"
+            onClick={increaseDate}
+          />
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Datepicker;
-        
